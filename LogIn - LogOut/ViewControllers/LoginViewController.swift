2b3ab73e6c0a5238c73e0c6ab6482c2c3d2ff481
Vladimir <p-bobka@mail.ru>
2020-12-16 01:34:27 +0300

    final version with all graphics and logic

--- LogIn - LogOut/ViewControllers/LoginViewController.swift
+++ LogIn - LogOut/ViewControllers/LoginViewController.swift
@@ -8,12 +8,68 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet var userNameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
+    private let user = User.getUserData()
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let tabBarController = segue.destination as! UITabBarController
+        let welcomeVC = tabBarController.viewControllers?.first as! WelcomeViewController
+        let navigationVC = tabBarController.viewControllers?.last as! UINavigationController
+        let aboutUserVC = navigationVC.topViewController as! AboutMeViewController
+        
+        welcomeVC.user = user
+        aboutUserVC.user = user
     }
-
-
+    @IBAction func logInPressed() {
+        guard
+            userNameTextField.text == user.login,
+            passwordTextField.text == user.password
+            else {
+            showAllert(title: "Invalid login or password", message: "Please enter correct login and password", textField: passwordTextField)
+            return
+        }
+        performSegue(withIdentifier: "logIn", sender: nil)
+    }
+    
+    @IBAction func forgotUserNamePressed() {
+        showAllert(title: "Oops!", message: "Your user name is \(user.login)")
+    }
+    @IBAction func forgotPasswordPressed() {
+        showAllert(title: "Oops!", message: "Your password is \(user.password)")
+    }
+    
+    @IBAction func unwindSegue(segue: UIStoryboardSegue) {
+        userNameTextField.text = nil
+        passwordTextField.text = nil
+    }
 }
-
+// MARK: - Allert Controller
+extension LoginViewController {
+    private func showAllert(title: String, message: String, textField: UITextField? = nil) {
+        let allert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) {_ in
+            textField?.text = nil}
+        allert.addAction(okAction)
+        present(allert, animated: true)
+    }
+}
+// MARK: - Text Fields Delegate
+extension LoginViewController: UITextFieldDelegate {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == userNameTextField {
+            passwordTextField.becomeFirstResponder()
+        }else{
+            logInPressed()
+        }
+        return true
+    }
+}
--- LogIn - LogOut/ViewControllers/WelcomeViewController.swift
+++ LogIn - LogOut/ViewControllers/WelcomeViewController.swift
@@ -9,4 +9,12 @@ import UIKit
 
 class WelcomeViewController: UIViewController {
     
+    @IBOutlet var welcomeLabel: UILabel!
+    
+    var user: User!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        welcomeLabel.text = "Welcome, \(user.person.fullName)!"
+    }
 }
